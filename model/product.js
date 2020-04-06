d3316914ce15cccc70792e1b6c3ae44ed15f1dde
CrisLaez <you@example.comcrislaez30@gmail.com>
2020-04-06 22:32:12 +0200

    pequeno cambio de modelo

--- model/product.js
+++ model/product.js
@@ -8,7 +8,7 @@ const ProductSchema = Schema({
     marca:String,
     foto:String,
     precio:{type:Number, default:0},
-    categorya:{type:String, enum: ['camiseta','pantalon','zapatos','sudadera','jersey','chaqueta']},
+    categoria:{type:String, enum: ['camiseta','pantalon','zapatos','sudadera','jersey','chaqueta']},
     talla:String,
     cantidad:Number
 })
